b6410c7434bc04502afd9607e0aab0f4c2eb7a41
asmaa2706 <60645217+asmaa2706@users.noreply.github.com>
2021-04-01 00:08:39 +0200

    Update url.php

--- url.php
+++ url.php
@@ -1,7 +1,9 @@
 <?php
 require_once(realpath(dirname(__FILE__) . 'config.php'));
+//Use to check if there is anything in our get methode and if yes, it's assign to our url variable.
 $url = isset($_GET['url']) ? $_GET['url'] : '';
 $db = new Connect;
+//Use to get the long url from the short url and redirect the short url to the long url.
 $getLongURL = $db -> prepare('SELECT long_url FROM shortened_urls WHERE short_url = :short_url');
 $getLongURL -> execute([
     'short_url' => $url
